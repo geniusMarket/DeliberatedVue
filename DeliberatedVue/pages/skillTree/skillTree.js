bfda67378c143ca9b27fe468abf25bddf6462b92
HHKKsdj <563589126@qq.com>
2021-11-12 23:07:48 +0800

    完成了技能树的绘制，对代码进行了整理

--- DeliberatedVue/pages/skillTree/skillTree.js
+++ DeliberatedVue/pages/skillTree/skillTree.js
@@ -1,66 +1,111 @@
-// pages/skillTree/skillTree.js
+import * as echarts from '../../components/ec-canvas/echarts'
+const app = getApp();
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    ec: {
+      onInit: initChart
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  onReady() {},
+  onshow() {}
+});
 
-  },
+function initChart(canvas, width, height, dpr) {
+  const chart = echarts.init(canvas, null, {
+    width: width,
+    height: height,
+    devicePixelRatio: dpr // new
+  });
+  canvas.setChart(chart);
+  var data1 = {
+    "name": "Vue",
+    "children": [{
+      "name": "混合"
+    }, {
+      "name": "组件"
+    }, {
+      "name": "方法与事件修饰器"
+    }, {
+      "name": "列表渲染"
+    }, {
+      "name": "class与style绑定"
+    }, {
+      "name": "条件渲染"
+    }, {
+      "name": "深入式响应原理"
+    }, {
+      "name": "过滤器"
+    }, {
+      "name": "路由"
+    }, {
+      "name": "状态管理"
+    }, {
+      "name": "总体特性"
+    }]
+  };
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  var option = {
+    series: [{
+      type: 'tree',
 
-  },
+      initialTreeDepth: 1,
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+      name: 'skillTree',
 
-  },
+      data: [data1],
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+      top: '5%',
+      left: '10%',
+      bottom: '2%',
+      right: '20%',
 
-  },
+      symbolSize: 17.5,
+      symbol: 'circle',
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+      label: {
+        normal: {
+          position: 'top',
+          verticalAlign: 'bottom',
+          align: 'middle',
+          color: 'white',
 
-  },
+        },
+        textStyle: {
+          fontSize: 30
+        }
+      }
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
 
-  },
+    }]
+  };
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+  chart.setOption(option);
+  chart.on("click", function treeNodeclick(param) {
+    /* true 代表点击的是圆点
+       fasle 表示点击的是当前节点的文本
+    */
 
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+    if (param.event.target.culling === true) {
+      let name = param.data.name;
+      let args = param.data; //当前节点及其子节点的值
+      let level = param.data.level; //当前节点的层级 
+      if (name != "Vue") {
+        wx.navigateTo({
+          url: 'childrenTree/childrenTree',
+        })
+      }
+    } else if (param.event.target.culling === false) {
+      let args = param.data; //当前节点及其子节点的值
+      let level = param.data.level; //当前节点的层级 eg:"1-1-0",可以通过level判断当前的层级，从而进行不同的操作
+      if (name != "Vue") {
+        wx.navigateTo({
+          url: 'childrenTree/childrenTree',
+        })
+      }
+    }
+  })
+  return chart;
+};
