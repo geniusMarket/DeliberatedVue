23d972714b109d9a20354ca9ab76b96156e81abe
cs0501 <960868236@qq.com>
2021-12-07 01:27:07 +0800

    完善了大多数功能
    完善了提问，采纳回答，我的提问页面绘制，文章与评论点赞等各种功能。

--- DeliberatedVue/pages/forum/posting/posting.js
+++ DeliberatedVue/pages/forum/posting/posting.js
@@ -1,55 +1,69 @@
 // pages/forum/posting/posting.js
 import {
-    addArticle
-  } from "../../../api/forum"
+	addArticle
+} from "../../../api/forum"
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-      detail:"",
-      title:"",
-    },
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
+		detail: "",
+		title: "",
+	},
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-        
-    },
-    // 发布一个帖子或修改帖子
-    addArticle() {
-        let data = {
-            "author":"attention_123",
-            "title":this.data.title,
-            "detail":this.data.detail,
-            "codeId": 0,
-            "type": 1
-        }
-        addArticle("POST", data, true).then(res => {
-          console.log(res)
-        }).catch(err => {
-          console.log(err)
-        })
-    },
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function (options) {
 
-    // 获取帖子具体内容
-    detailInput:function(e){
-      this.data.detail = e.detail.value,
-      this.setData({
-        detail: this.data.detail,
-      })
-    },
-    // 获取帖子标题
-    titleInput:function(e){
-      this.data.title = e.detail.value,
-      this.setData({
-        title: this.data.title,
-      })
-    },
-    // 发布帖子
-    sendPost(){
-      this.addArticle();
-    }
+	},
+	// 发布一个帖子或修改帖子
+	addArticle() {
+		let data = {
+			"author": wx.getStorageSync('openId'),
+			"title": this.data.title,
+			"detail": this.data.detail,
+			"codeId": 0,
+			"type": 1
+		}
+		addArticle("POST", data, true).then(res => {
+			console.log(res)
+		}).catch(err => {
+			console.log(err)
+		})
+	},
+
+	// 获取帖子具体内容
+	detailInput: function (e) {
+		this.setData({
+			detail: e.detail.value
+		})
+	},
+	// 获取帖子标题
+	titleInput: function (e) {
+		this.setData({
+			title: e.detail.value,
+		})
+	},
+	// 发布帖子
+	sendPost() {
+		if (this.data.title && this.data.detail) {
+			this.addArticle();
+			wx.navigateTo({
+			  url: '../forum.js',
+			})
+		} else if (this.data.title) wx.showToast({
+			title: '帖子的内容不能为空',
+			icon: 'none'
+		})
+		else if (this.data.detail) wx.showToast({
+			title: '帖子的标题不能为空',
+			icon: 'none'
+		})
+		else wx.showToast({
+			title: '帖子的标题和内容均不能为空',
+			icon: 'none'
+		})
+	}
 })
